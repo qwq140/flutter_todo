ace7df64471df04c0b75affd26202e3166f0f843
JB <qwq140@gmail.com>
2022-10-12 10:23:31 +0900

    list_page create

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/app/screen/list_page.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({Key? key}) : super(key: key);
@@ -6,7 +7,8 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      home: ListPage(),
     );
   }
 }
---
+++ lib/app/screen/list_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('리스트 페이지'),
+      ),
+    );
+  }
+}
